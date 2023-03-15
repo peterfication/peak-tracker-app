52d971d58d6e592a13a5d76129c095f33bc09414
Peter Gundel <mail@petergundel.de>
2023-03-15 16:23:18 +0100

    Add @typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     'prettier/prettier': 0,
+    '@typescript-eslint/no-empty-function': 0,
   },
   ignorePatterns: ['node_modules/', 'cache/', 'coverage/', 'dist/', 'build/'],
 };
