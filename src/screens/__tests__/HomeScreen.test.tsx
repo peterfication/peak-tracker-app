4a8bb1838d5c2fe86d601e695321aae69a43a4b2
Peter Gundel <mail@petergundel.de>
2023-03-20 06:58:10 +0100

    Improve mocks in testing

--- src/screens/__tests__/HomeScreen.test.tsx
+++ src/screens/__tests__/HomeScreen.test.tsx
@@ -2,18 +2,23 @@ import { fireEvent, render, screen } from '@testing-library/react-native';
 import React from 'react';
 
 import { AuthContext } from '../../contexts/AuthContext';
+import { useAuth } from '../../hooks/useAuth';
 import { HomeScreen } from '../HomeScreen';
 
-jest.mock('../../hooks/useAuth', () => ({
-  useAuth: () => ({
-    login: jest.fn(),
-    logout: jest.fn(),
-    isAuthenticated: true,
-  }),
-}));
+jest.mock('../../hooks/useAuth');
+const mockedUseAuth = jest.mocked(useAuth);
 
 describe('HomeScreen', () => {
-  const logout = jest.fn();
+  const mockedLogout = jest.fn();
+
+  beforeEach(() => {
+    mockedUseAuth.mockReturnValue({
+      login: jest.fn(),
+      logout: mockedLogout,
+      authLoading: false,
+      isAuthenticated: true,
+    });
+  });
 
   afterEach(() => {
     jest.clearAllMocks();
@@ -29,12 +34,16 @@ describe('HomeScreen', () => {
   it('should call the logout function when the Login button is pressed', () => {
     render(
       <AuthContext.Provider
-        value={{ logout, authLoading: false, isAuthenticated: true }}>
+        value={{
+          logout: mockedLogout,
+          authLoading: false,
+          isAuthenticated: true,
+        }}>
         <HomeScreen />
       </AuthContext.Provider>,
     );
     const logoutButton = screen.getByTestId('logout-button');
     fireEvent.press(logoutButton);
-    expect(logout).toHaveBeenCalledTimes(1);
+    expect(mockedLogout).toHaveBeenCalledTimes(1);
   });
 });
