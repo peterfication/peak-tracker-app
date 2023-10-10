5c93dc7b310073558fa523cc2d4fbf183f5c3d92
Peter Morgenstern <mail@petergundel.de>
2023-10-10 08:09:45 +0200

    Update GraphQL Codegen/Inspector packages

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -5,60 +5,62 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
 };
 
 /** A relay node */
 export type Node = {
   /** A unique identifier */
-  id: Scalars['ID'];
+  id: Scalars['ID']['output'];
 };
 
 /** A relay page info */
 export type PageInfo = {
   __typename?: 'PageInfo';
   /** When paginating forwards, the cursor to continue */
-  endCursor?: Maybe<Scalars['String']>;
+  endCursor?: Maybe<Scalars['String']['output']>;
   /** When paginating forwards, are there more items? */
-  hasNextPage: Scalars['Boolean'];
+  hasNextPage: Scalars['Boolean']['output'];
   /** When paginating backwards, are there more items? */
-  hasPreviousPage: Scalars['Boolean'];
+  hasPreviousPage: Scalars['Boolean']['output'];
   /** When paginating backwards, the cursor to continue */
-  startCursor?: Maybe<Scalars['String']>;
+  startCursor?: Maybe<Scalars['String']['output']>;
 };
 
 export type Peak = Node & {
   __typename?: 'Peak';
-  elevation: Scalars['Int'];
-  id: Scalars['ID'];
-  latitude: Scalars['Float'];
-  longitude: Scalars['Float'];
-  name: Scalars['String'];
-  osmId: Scalars['Int'];
-  scaleCount: Scalars['Int'];
-  scaledByUser?: Maybe<Scalars['Boolean']>;
-  slug: Scalars['String'];
-  wikidataId?: Maybe<Scalars['String']>;
-  wikipedia?: Maybe<Scalars['String']>;
+  elevation: Scalars['Int']['output'];
+  id: Scalars['ID']['output'];
+  latitude: Scalars['Float']['output'];
+  longitude: Scalars['Float']['output'];
+  name: Scalars['String']['output'];
+  osmId: Scalars['Int']['output'];
+  scaleCount: Scalars['Int']['output'];
+  scaledByUser?: Maybe<Scalars['Boolean']['output']>;
+  slug: Scalars['String']['output'];
+  wikidataId?: Maybe<Scalars['String']['output']>;
+  wikipedia?: Maybe<Scalars['String']['output']>;
 };
 
 
 export type PeakScaledByUserArgs = {
-  userId: Scalars['ID'];
+  userId: Scalars['ID']['input'];
 };
 
 /** :peak connection */
 export type PeakConnection = {
   __typename?: 'PeakConnection';
   /** Total count on all pages */
-  count?: Maybe<Scalars['Int']>;
+  count?: Maybe<Scalars['Int']['output']>;
   /** :peak edges */
   edges?: Maybe<Array<Maybe<PeakEdge>>>;
   /** Page information */
@@ -69,31 +71,31 @@ export type PeakConnection = {
 export type PeakEdge = {
   __typename?: 'PeakEdge';
   /** Cursor */
-  cursor: Scalars['String'];
+  cursor: Scalars['String']['output'];
   /** :peak node */
   node?: Maybe<Peak>;
 };
 
 export type PeakFilterElevation = {
-  eq?: InputMaybe<Scalars['Int']>;
-  greaterThan?: InputMaybe<Scalars['Int']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['Int']>;
-  in?: InputMaybe<Array<Scalars['Int']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['Int']>;
-  lessThanOrEqual?: InputMaybe<Scalars['Int']>;
-  notEq?: InputMaybe<Scalars['Int']>;
+  eq?: InputMaybe<Scalars['Int']['input']>;
+  greaterThan?: InputMaybe<Scalars['Int']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['Int']['input']>;
+  in?: InputMaybe<Array<Scalars['Int']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['Int']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['Int']['input']>;
+  notEq?: InputMaybe<Scalars['Int']['input']>;
 };
 
 export type PeakFilterId = {
-  eq?: InputMaybe<Scalars['ID']>;
-  greaterThan?: InputMaybe<Scalars['ID']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['ID']>;
-  in?: InputMaybe<Array<Scalars['ID']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['ID']>;
-  lessThanOrEqual?: InputMaybe<Scalars['ID']>;
-  notEq?: InputMaybe<Scalars['ID']>;
+  eq?: InputMaybe<Scalars['ID']['input']>;
+  greaterThan?: InputMaybe<Scalars['ID']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['ID']['input']>;
+  in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['ID']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['ID']['input']>;
+  notEq?: InputMaybe<Scalars['ID']['input']>;
 };
 
 export type PeakFilterInput = {
@@ -113,107 +115,107 @@ export type PeakFilterInput = {
 };
 
 export type PeakFilterLatitude = {
-  eq?: InputMaybe<Scalars['Float']>;
-  greaterThan?: InputMaybe<Scalars['Float']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['Float']>;
-  in?: InputMaybe<Array<Scalars['Float']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['Float']>;
-  lessThanOrEqual?: InputMaybe<Scalars['Float']>;
-  notEq?: InputMaybe<Scalars['Float']>;
+  eq?: InputMaybe<Scalars['Float']['input']>;
+  greaterThan?: InputMaybe<Scalars['Float']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['Float']['input']>;
+  in?: InputMaybe<Array<Scalars['Float']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['Float']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['Float']['input']>;
+  notEq?: InputMaybe<Scalars['Float']['input']>;
 };
 
 export type PeakFilterLongitude = {
-  eq?: InputMaybe<Scalars['Float']>;
-  greaterThan?: InputMaybe<Scalars['Float']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['Float']>;
-  in?: InputMaybe<Array<Scalars['Float']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['Float']>;
-  lessThanOrEqual?: InputMaybe<Scalars['Float']>;
-  notEq?: InputMaybe<Scalars['Float']>;
+  eq?: InputMaybe<Scalars['Float']['input']>;
+  greaterThan?: InputMaybe<Scalars['Float']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['Float']['input']>;
+  in?: InputMaybe<Array<Scalars['Float']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['Float']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['Float']['input']>;
+  notEq?: InputMaybe<Scalars['Float']['input']>;
 };
 
 export type PeakFilterName = {
-  eq?: InputMaybe<Scalars['String']>;
-  greaterThan?: InputMaybe<Scalars['String']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['String']>;
-  in?: InputMaybe<Array<Scalars['String']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['String']>;
-  lessThanOrEqual?: InputMaybe<Scalars['String']>;
-  notEq?: InputMaybe<Scalars['String']>;
+  eq?: InputMaybe<Scalars['String']['input']>;
+  greaterThan?: InputMaybe<Scalars['String']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  in?: InputMaybe<Array<Scalars['String']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['String']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  notEq?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type PeakFilterOsmId = {
-  eq?: InputMaybe<Scalars['Int']>;
-  greaterThan?: InputMaybe<Scalars['Int']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['Int']>;
-  in?: InputMaybe<Array<Scalars['Int']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['Int']>;
-  lessThanOrEqual?: InputMaybe<Scalars['Int']>;
-  notEq?: InputMaybe<Scalars['Int']>;
+  eq?: InputMaybe<Scalars['Int']['input']>;
+  greaterThan?: InputMaybe<Scalars['Int']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['Int']['input']>;
+  in?: InputMaybe<Array<Scalars['Int']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['Int']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['Int']['input']>;
+  notEq?: InputMaybe<Scalars['Int']['input']>;
 };
 
 export type PeakFilterScaleCount = {
-  eq?: InputMaybe<Scalars['Int']>;
-  greaterThan?: InputMaybe<Scalars['Int']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['Int']>;
-  in?: InputMaybe<Array<InputMaybe<Scalars['Int']>>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['Int']>;
-  lessThanOrEqual?: InputMaybe<Scalars['Int']>;
-  notEq?: InputMaybe<Scalars['Int']>;
+  eq?: InputMaybe<Scalars['Int']['input']>;
+  greaterThan?: InputMaybe<Scalars['Int']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['Int']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['Int']['input']>>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['Int']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['Int']['input']>;
+  notEq?: InputMaybe<Scalars['Int']['input']>;
 };
 
 export type PeakFilterScaledByUser = {
-  eq?: InputMaybe<Scalars['Boolean']>;
-  greaterThan?: InputMaybe<Scalars['Boolean']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['Boolean']>;
-  in?: InputMaybe<Array<Scalars['Boolean']>>;
+  eq?: InputMaybe<Scalars['Boolean']['input']>;
+  greaterThan?: InputMaybe<Scalars['Boolean']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['Boolean']['input']>;
+  in?: InputMaybe<Array<Scalars['Boolean']['input']>>;
   input?: InputMaybe<PeakScaledByUserFieldInput>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['Boolean']>;
-  lessThanOrEqual?: InputMaybe<Scalars['Boolean']>;
-  notEq?: InputMaybe<Scalars['Boolean']>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['Boolean']['input']>;
+  notEq?: InputMaybe<Scalars['Boolean']['input']>;
 };
 
 export type PeakFilterSlug = {
-  eq?: InputMaybe<Scalars['String']>;
-  greaterThan?: InputMaybe<Scalars['String']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['String']>;
-  in?: InputMaybe<Array<Scalars['String']>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['String']>;
-  lessThanOrEqual?: InputMaybe<Scalars['String']>;
-  notEq?: InputMaybe<Scalars['String']>;
+  eq?: InputMaybe<Scalars['String']['input']>;
+  greaterThan?: InputMaybe<Scalars['String']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  in?: InputMaybe<Array<Scalars['String']['input']>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['String']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  notEq?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type PeakFilterWikidataId = {
-  eq?: InputMaybe<Scalars['String']>;
-  greaterThan?: InputMaybe<Scalars['String']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['String']>;
-  in?: InputMaybe<Array<InputMaybe<Scalars['String']>>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['String']>;
-  lessThanOrEqual?: InputMaybe<Scalars['String']>;
-  notEq?: InputMaybe<Scalars['String']>;
+  eq?: InputMaybe<Scalars['String']['input']>;
+  greaterThan?: InputMaybe<Scalars['String']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['String']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  notEq?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type PeakFilterWikipedia = {
-  eq?: InputMaybe<Scalars['String']>;
-  greaterThan?: InputMaybe<Scalars['String']>;
-  greaterThanOrEqual?: InputMaybe<Scalars['String']>;
-  in?: InputMaybe<Array<InputMaybe<Scalars['String']>>>;
-  isNil?: InputMaybe<Scalars['Boolean']>;
-  lessThan?: InputMaybe<Scalars['String']>;
-  lessThanOrEqual?: InputMaybe<Scalars['String']>;
-  notEq?: InputMaybe<Scalars['String']>;
+  eq?: InputMaybe<Scalars['String']['input']>;
+  greaterThan?: InputMaybe<Scalars['String']['input']>;
+  greaterThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  isNil?: InputMaybe<Scalars['Boolean']['input']>;
+  lessThan?: InputMaybe<Scalars['String']['input']>;
+  lessThanOrEqual?: InputMaybe<Scalars['String']['input']>;
+  notEq?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type PeakScaledByUserFieldInput = {
-  userId: Scalars['ID'];
+  userId: Scalars['ID']['input'];
 };
 
 export enum PeakSortField {
@@ -245,17 +247,17 @@ export type RootQueryType = {
 
 
 export type RootQueryTypePeakArgs = {
-  id: Scalars['ID'];
+  id: Scalars['ID']['input'];
 };
 
 
 export type RootQueryTypePeaksArgs = {
-  after?: InputMaybe<Scalars['String']>;
-  before?: InputMaybe<Scalars['String']>;
+  after?: InputMaybe<Scalars['String']['input']>;
+  before?: InputMaybe<Scalars['String']['input']>;
   filter?: InputMaybe<PeakFilterInput>;
-  first?: InputMaybe<Scalars['Int']>;
-  last?: InputMaybe<Scalars['Int']>;
-  scaledByActor?: InputMaybe<Scalars['Boolean']>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  last?: InputMaybe<Scalars['Int']['input']>;
+  scaledByActor?: InputMaybe<Scalars['Boolean']['input']>;
   sort?: InputMaybe<Array<InputMaybe<PeakSortInput>>>;
 };
 
@@ -270,8 +272,8 @@ export enum SortOrder {
 
 export type User = {
   __typename?: 'User';
-  email: Scalars['String'];
-  id: Scalars['ID'];
+  email: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
 };
 
 export type GetPeaksQueryVariables = Exact<{ [key: string]: never; }>;
