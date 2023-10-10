b4aac6582fbcfe087640a6388fff2f1eb3273938
Peter Morgenstern <mail@petergundel.de>
2023-10-10 10:13:11 +0200

    Improve Metro config for Storybook

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,13 @@
+const crypto = require('crypto');
+
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+// See https://github.com/goatandsheep/react-native-dotenv/issues/75#issuecomment-704320755
+// Make sure that different APP_ENV values lead to a reset of the cache. This is
+// for example necessary when you switch between development and storybook
+let hash = crypto.createHash('sha256');
+hash.update(`${process.env.APP_ENV}`);
+const cacheVersion = hash.digest('hex');
 
 /**
  * Metro configuration
@@ -6,7 +15,9 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  cacheVersion,
+};
 
 const mergedConfig = mergeConfig(getDefaultConfig(__dirname), config);
 
