b346f505341826cd5213a5a4d1eb60e255f3fd39
Peter Morgenstern <mail@petergundel.de>
2023-10-10 14:08:52 +0200

    Improve DevMenu loading
    So that the DevMenu and Storybook won't get bundled into the production
    build.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { addItem } from 'react-native-dev-menu';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import {
   ApolloProvider,
@@ -7,29 +6,31 @@ import {
   NavigationProvider,
 } from '@app/providers';
 
-import Storybook from './.storybook/Storybook';
-
-/**
- * Add item to DevMenu (Cmd+D) to toggle between the app and Storybook.
- */
-const addDevMenuItemForStorybook = (
-  setShowStorybook: React.Dispatch<React.SetStateAction<boolean>>,
-  showStorybook: boolean,
-) => {
-  addItem('Toggle Storybook', () => setShowStorybook(!showStorybook)).catch(
-    error =>
-      error instanceof Error &&
-      console.error(
-        `DevMenu.addItem "Toggle Storybook" error: ${error.toString()}`,
-      ),
-  );
-};
-
 export const App = () => {
   const [showStorybook, setShowStorybook] = useState(false);
+  const toggleStorybook = useCallback(
+    () => setShowStorybook(previousState => !previousState),
+    [],
+  );
+
+  useEffect(
+    () => {
+      if (__DEV__) {
+        // Add item to DevMenu (Cmd+D) to toggle between the app and Storybook.
+        // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment
+        const DevMenu = require('react-native-dev-menu');
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+        DevMenu.addItem('Toggle Storybook', toggleStorybook);
+      }
+    },
+    // We only want to run this hook once.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 
   if (__DEV__) {
-    addDevMenuItemForStorybook(setShowStorybook, showStorybook);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const Storybook = require('./.storybook/Storybook').default;
 
     if (showStorybook) {
       return <Storybook />;
